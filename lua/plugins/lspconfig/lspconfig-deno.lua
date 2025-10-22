2f85fd3bb5de5241c0d5c436984457c0e7b97c98
kenbo <kenbo@r2d2.kentaro.io>
2025-10-23 02:02:57 +0900

    added typescript-language-server.

--- lua/plugins/lspconfig/lspconfig-deno.lua
+++ lua/plugins/lspconfig/lspconfig-deno.lua
@@ -1,8 +1,13 @@
 local cmd = vim.fn.stdpath('data') .. "/mason/bin/deno";
 vim.lsp.config['deno'] = {
-    cmd = {cmd}, 
-    filetypes = {'javascript', 'typescript', 'js'},
-    rootmarker = {},
-    settings = {},
+    cmd = {cmd},
+    filetypes = {
+        'javascript', 'typescript', 'js'
+    },
+    root_dir = vim.fs.dirname(vim.fs.find({ ".git", "mvnw" }, { upward = true })[1]),
+    settings = {
+        enable = true,
+    },
 };
+
 vim.lsp.enable('deno');
---
+++ lua/plugins/lspconfig/typescript-language-server.lua
@@ -0,0 +1,14 @@
+local cmd = vim.fn.stdpath"data" .. "/mason/bin/typescript-language-server";
+
+vim.lsp.config['typescript-language-server'] = {
+    cmd = {
+        cmd,
+        "--stdio"
+    },
+
+    root_fir = vim.fs.dirname(vim.fs.find({'.git'}, { upward = true })[1]),
+    filetypes = {'javascript', 'typescript'},
+    settings = {},
+};
+
+vim.lsp.enable("typescript-language-server");
